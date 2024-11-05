4188d8c731dac53d485c410e3f447dda84a80369
anshifaanshi <anshifajamsher@gmail.com>
2024-11-05 11:59:32 +0300

    fix

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const allowedOrigins = [
     'https://food-frontend-me.vercel.app',
     'https://food-frontend-rust.vercel.app',
     'http://localhost:5173',
-    'https://food-frontend-ten.vercel.app'
+    'https://food-frontend-ten.vercel.app',
+    'https://food-frontend-gi3i.vercel.app'
 ];
 
 app.use(cors({
