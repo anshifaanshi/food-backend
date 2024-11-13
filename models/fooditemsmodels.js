05ed560e93cbc800f0f93d1fe4f9cb44c03b70d2
anshifaanshi <anshifajamsher@gmail.com>
2024-11-13 08:19:56 +0300

    fix

--- models/fooditemsmodels.js
+++ models/fooditemsmodels.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const fooditemsSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    
   },
   description: {
     type: String
   },
   price: {
     type: Number,
-    required: true
+    
   },
   availability: {
     type: Boolean,
